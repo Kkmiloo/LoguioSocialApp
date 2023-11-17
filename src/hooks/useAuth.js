4f6c5d3b8c1a8abeb93746f6ea30fe63d25ac1ea
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2023-11-16 22:53:23 -0500

    refact: funtion to arrow funtion

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -10,32 +10,32 @@ import { auth } from '../../config/firebaseConfig';
 export default function useAuth() {
   const [error, setError] = useState(null);
 
-  async function signUp(email, password) {
+  const signUp = async (email, password) => {
     try {
       await createUserWithEmailAndPassword(auth, email, password);
     } catch (e) {
       console.error('Error al registrar el usuario:', e.message);
       setError(e);
     }
-  }
+  };
 
-  async function signIn(email, password) {
+  const signIn = async (email, password) => {
     try {
       await signInWithEmailAndPassword(auth, email, password);
     } catch (e) {
       console.error('Error al iniciar sesión:', e.message);
       setError(e);
     }
-  }
+  };
 
-  async function logout() {
+  const logout = async () => {
     try {
       await firebaseSignOut(auth);
     } catch (e) {
       console.error('Error al cerrar sesión:', e.message);
       setError(e);
     }
-  }
+  };
 
   return {
     error,
