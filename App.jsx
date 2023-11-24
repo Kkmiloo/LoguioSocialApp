7eeca701dd2aff6d3285c7d0813a6b5af68d91c6
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2023-11-24 17:44:29 -0500

    add: font

--- App.jsx
+++ App.jsx
@@ -1,15 +1,40 @@
 import { NavigationContainer } from '@react-navigation/native';
-
-import React from 'react';
-
+import { useFonts } from 'expo-font';
+import React, { useCallback, useEffect } from 'react';
 import AppNavigator from './src/navigation/MainNavigator';
 import { Provider } from 'react-redux';
 import store from './src/redux';
+import * as SplashScreen from 'expo-splash-screen';
+import { View } from 'react-native';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Dhyana: require('./src/assets/fonts/Dhyana-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <AppNavigator />
+        <View onLayout={onLayout} style={{ flex: 1 }}>
+          <AppNavigator />
+        </View>
       </NavigationContainer>
     </Provider>
   );
