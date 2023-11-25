72d7a7d464a0f45a08bc231815e18e5efe84618d
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2023-11-24 19:35:29 -0500

    add: test Home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import useAuth from '../hooks/useAuthAction';
+import { useAppSelector } from '../hooks/store';
 
 function HomeScreen() {
   const { logout } = useAuth();
+  const { user } = useAppSelector((state) => state.user);
+
+  const parserUser = JSON.parse(user);
   const handleLogout = () => {
-    console.log('logout');
     logout();
   };
   return (
     <View>
-      <Text>Welcome to the Home Screen!</Text>
+      <Text>Welcome to the Home Screen! {parserUser.email} </Text>
       <Button title="LogOut" onPress={handleLogout} />
     </View>
   );
