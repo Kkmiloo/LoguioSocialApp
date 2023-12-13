cc77a56e60a756b5ee08ab11752036c1d39b7e7f
kkmiloo <camilo.reyes_granada@uao.edu.co>
2023-12-13 17:24:06 -0500

    add: catching errors

--- src/hooks/useAuthAction.js
+++ src/hooks/useAuthAction.js
@@ -8,7 +8,7 @@ import {
 
 import { auth, database } from '../config/firebaseConfig';
 import { useAppDispatch } from './store';
-import { loginUser, logoutUser } from '../redux/auth/authSlice';
+import { loginUser, logoutUser, setError } from '../redux/auth/authSlice';
 import { doc, setDoc } from 'firebase/firestore';
 
 export default function useAuth() {
@@ -30,21 +30,28 @@ export default function useAuth() {
   }, [dispatch]); */
 
   const signUp = async ({ email, password }) => {
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = JSON.stringify(userCredential.user);
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = JSON.stringify(userCredential.user);
 
-    await setDoc(doc(database, 'users', userCredential.user.uid), {
-      email: userCredential.user.email,
-      uid: userCredential.user.uid,
-    });
-
-    dispatch(loginUser(user));
+      await setDoc(doc(database, 'users', userCredential.user.uid), {
+        email: userCredential.user.email,
+        uid: userCredential.user.uid,
+      });
+      dispatch(loginUser(user));
+    } catch (error) {
+      dispatch(setError(error.message));
+    }
   };
 
   const signIn = async ({ email, password }) => {
-    const userCredential = await signInWithEmailAndPassword(auth, email, password);
-    const user = JSON.stringify(userCredential.user);
-    dispatch(loginUser(user));
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      const user = JSON.stringify(userCredential.user);
+      dispatch(loginUser(user));
+    } catch (error) {
+      dispatch(setError(error.message));
+    }
   };
 
   const logout = async () => {
