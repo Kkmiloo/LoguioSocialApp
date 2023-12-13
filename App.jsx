e5df931ff19b1f306761fa8df95152026eef032e
kkmiloo <camilo.reyes_granada@uao.edu.co>
2023-12-13 17:21:10 -0500

    fix: font problems

--- App.jsx
+++ App.jsx
@@ -1,42 +1,42 @@
 /* global require */
 import { NavigationContainer } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import AppNavigator from './src/navigation/MainNavigator';
 import { Provider } from 'react-redux';
 import store from './src/redux';
 import * as SplashScreen from 'expo-splash-screen';
 import { View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
+  const [fontsLoaded, fontError] = useFonts({
     Dhyana: require('./src/assets/fonts/Dhyana-Regular.ttf'),
   });
 
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await SplashScreen.preventAutoHideAsync();
-      } catch (e) {
-        console.warn(e);
-      }
-    }
-    prepare();
-  }, []);
-
-  const onLayout = useCallback(async () => {
-    if (fontsLoaded) {
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
       await SplashScreen.hideAsync();
     }
-  }, [fontsLoaded]);
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return (
-    <Provider store={store}>
-      <NavigationContainer>
-        <View onLayout={onLayout} style={{ flex: 1 }}>
-          <AppNavigator />
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <SafeAreaView style={{ flex: 1 }}>
+        <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+          <Provider store={store}>
+            <NavigationContainer>
+              <AppNavigator />
+            </NavigationContainer>
+          </Provider>
         </View>
-      </NavigationContainer>
-    </Provider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
