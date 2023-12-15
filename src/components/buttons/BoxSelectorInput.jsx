a1787086323af922f75eb37af5323c20e558fe0c
kkmiloo <camilo.reyes_granada@uao.edu.co>
2023-12-15 17:18:54 -0500

    add: basic components

---
+++ src/components/buttons/BoxSelectorInput.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
+function CheckboxInput({ isChecked, onValueChange, label }) {
+  return (
+    <View style={styles.checkboxContainer}>
+      <CheckBox value={isChecked} onValueChange={onValueChange} style={styles.checkbox} />
+      <Text style={styles.label}>{label}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  checkboxContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+  checkbox: {
+    alignSelf: 'center',
+  },
+  label: {
+    margin: 8,
+  },
+});
+
+export default CheckboxInput;
