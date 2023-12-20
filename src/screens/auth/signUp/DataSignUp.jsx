8885b8cdfbeedf3d156d3438408d2855a5347566
kkmiloo <camilo.reyes_granada@uao.edu.co>
2023-12-20 17:27:00 -0500

    Add ScrollView and additional activities in DataSignUp component

--- src/screens/auth/signUp/DataSignUp.jsx
+++ src/screens/auth/signUp/DataSignUp.jsx
@@ -1,49 +1,56 @@
-import { View, Text, Image, Pressable } from 'react-native';
+import { View, Text, Image, Pressable, ScrollView } from 'react-native';
 import React, { useState } from 'react';
 import loguioLogo from '../../../assets/images/loguio.png';
 import { styles } from './SignUp.styles';
 import AppTextInput from '../../../components/inputs/AppTextInput';
-import CheckboxInput from '../../../components/buttons/BoxSelectorInput';
+import BoxSelectorInput from '../../../components/buttons/BoxSelectorInput';
 
 function DataSignUp() {
   const [name, setName] = useState('');
   const [lastName, setLastName] = useState('');
-  const [birthDate, setBirthDate] = useState('');
   const [phone, setPhone] = useState('');
+  const [date, setDate] = useState('');
 
-  const data = ['actividad 1', 'actividad 2', 'actividad 3', 'actividad 4'];
+  const data = [
+    'actividad 1',
+    'actividad 2',
+    'actividad 3',
+    'actividad 4',
+    'actividad 5',
+    'actividad 6',
+  ];
 
   const initialCheckboxState = data.reduce((acc, curr) => ({ ...acc, [curr]: false }), {});
   const [checkboxes, setCheckboxes] = useState(initialCheckboxState);
   const handleValueChange = (item) => {
     setCheckboxes((prev) => ({ ...prev, [item]: !prev[item] }));
   };
+
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Image style={styles.logo} source={loguioLogo} />
       <Text style={{ fontSize: 24, marginBottom: 24 }}> Define tu rol </Text>
       <AppTextInput placeholder={'Nombre'} value={name} onChangeText={setName} />
       <AppTextInput placeholder={'Apellidos'} value={lastName} onChangeText={setLastName} />
-      <AppTextInput
-        placeholder={'fecha nacimiento'}
-        value={birthDate}
-        onChangeText={setBirthDate}
-      />
-      <AppTextInput placeholder={'telefono'} value={phone} onChangeText={setPhone} />
 
-      {data.map((item) => (
-        <CheckboxInput
-          key={item}
-          label={item}
-          isChecked={checkboxes[item]}
-          onValueChange={() => handleValueChange(item)}
-        />
-      ))}
+      <AppTextInput placeholder={'Fecha de nacimiento'} value={date} onChangeText={setDate} />
+      <AppTextInput placeholder={'telefono'} value={phone} onChangeText={setPhone} />
       <Text style={{ fontSize: 18, marginBottom: 24 }}> añade tus actividades preferidas </Text>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', flexWrap: 'wrap' }}>
+        {data.map((item) => (
+          <BoxSelectorInput
+            style={{ width: '50%', display: 'flex', flexDirection: 'row' }}
+            key={item}
+            label={item}
+            isChecked={checkboxes[item]}
+            onValueChange={() => handleValueChange(item)}
+          />
+        ))}
+      </View>
       <Pressable style={styles.button}>
         <Text style={styles.textButton}> Continuar </Text>
       </Pressable>
-    </View>
+    </ScrollView>
   );
 }
 
