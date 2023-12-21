fc52179f319060b420763aea2e672afef493b0df
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2023-12-20 23:06:15 -0500

    Add images and update radio buttons in RoleSignUp component

--- src/screens/auth/signUp/RoleSignUp.jsx
+++ src/screens/auth/signUp/RoleSignUp.jsx
@@ -3,12 +3,19 @@ import React, { useState } from 'react';
 import RadioBtn from '../../../components/buttons/RadioBtn';
 import { styles } from './SignUp.styles';
 import loguioLogo from '../../../assets/images/loguio.png';
+import guide from '../../../assets/images/guide.png';
+import interpreter from '../../../assets/images/interpreter.png';
+import tourist from '../../../assets/images/tourist.png';
 import { useNavigation } from '@react-navigation/native';
 function RoleSignUp() {
   const [value, setValue] = useState('');
 
   const navigation = useNavigation();
-  const data = ['Turista', 'Guia', 'Interprete'];
+  const data = [
+    { role: 'Turista', img: tourist },
+    { role: 'Guía', img: guide },
+    { role: 'Interprete', img: interpreter },
+  ];
 
   const handleContinue = () => {
     if (!value) return;
@@ -19,15 +26,27 @@ function RoleSignUp() {
       <Image style={styles.logo} source={loguioLogo} />
       <Text style={{ fontSize: 24, marginBottom: 24 }}> Define tu rol </Text>
 
-      {data.map((item) => (
-        <RadioBtn
-          key={item}
-          label={item}
-          value={item}
-          onValueChange={setValue}
-          selectedValue={value}
-        />
-      ))}
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          width: '100%',
+          justifyContent: 'space-around',
+        }}
+      >
+        {data.map((item) => (
+          <View key={item.role} style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Image style={{ marginBottom: 7 }} source={item.img} />
+
+            <RadioBtn
+              label={item.role}
+              value={item.role}
+              onValueChange={setValue}
+              selectedValue={value}
+            />
+          </View>
+        ))}
+      </View>
 
       <Pressable style={styles.button} onPress={handleContinue}>
         <Text style={styles.textButton}> Continuar </Text>
