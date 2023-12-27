f2046f02384d72ed702bce70a384a64d0f8f5c08
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2023-12-26 22:55:48 -0500

    Add useEffect hook to navigate to RoleSignUp if user profile is not completed

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
 import useAuth from '../hooks/useAuthAction';
-import { useAppSelector } from '../hooks/store';
+import { useAppDispatch, useAppSelector } from '../hooks/store';
+import { handleAuthStateChanged } from '../redux/auth/authSlice';
+import { useNavigation } from '@react-navigation/native';
 
 function HomeScreen() {
   const { logout } = useAuth();
   const { user } = useAppSelector((state) => state.auth);
 
-  const parserUser = JSON.parse(user);
+  const navigation = useNavigation();
+  useEffect(() => {
+    if (user && !user.profileCompleted) {
+      navigation.navigate('RoleSignUp');
+    }
+  }, [user, navigation]);
   const handleLogout = () => {
     logout();
   };
   return (
     <View>
-      <Text>Welcome to the Home Screen! {parserUser.email} </Text>
+      <Text>Welcome to the Home Screen! {user.email} </Text>
       <Button title="LogOut" onPress={handleLogout} />
     </View>
   );
