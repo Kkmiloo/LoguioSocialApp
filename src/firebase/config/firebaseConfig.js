d2e8bdbea52c1a93e937d351d15ef50fa2a6ce03
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2023-12-26 22:54:16 -0500

    Add Firebase configuration file

--- src/firebase/config/firebaseConfig.js
+++ src/firebase/config/firebaseConfig.js
@@ -10,6 +10,12 @@ const firebaseConfig = {
   storageBucket: Constants.expoConfig.extra.storageBucket,
   messagingSenderId: Constants.expoConfig.extra.messagingSenderId,
   appId: Constants.expoConfig.extra.appId,
+  /*   apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID, */
 };
 
 initializeApp(firebaseConfig);
