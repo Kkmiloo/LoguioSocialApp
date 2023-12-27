73ec2f828d747bf76a681859983cfeb14b9873b8
Camilo Reyes <Camilo.reyes_granada@uao.edu.co>
2023-12-26 22:55:11 -0500

    Add RoleSignUp screen to AuthNavigator

--- src/navigation/AuthNavigator.jsx
+++ src/navigation/AuthNavigator.jsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LoginScreen from '../screens/auth/login/LoginScreen';
 import SignUpScreen from '../screens/auth/signUp/SignUpScreen';
 import DataSignUp from '../screens/auth/signUp/DataSignUp';
+import RoleSignUp from '../screens/auth/signUp/RoleSignUp';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ function AuthNavigator() {
     <Stack.Navigator initialRouteName="login">
       <Stack.Screen name="login" component={LoginScreen} options={{ headerShown: false }} />
       <Stack.Screen name="signUp" component={SignUpScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="RoleSignUp" component={RoleSignUp} options={{ headerShown: false }} />
       <Stack.Screen name="DataSignUp" component={DataSignUp} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
