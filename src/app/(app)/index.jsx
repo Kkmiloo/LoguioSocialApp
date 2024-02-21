9a5f68f94891b49374a1a2a2fa3976f0844866a7
Camilo <camilo.reyes_granada@uao.edu.co>
2024-02-21 15:43:03 -0500

    add: SignOut btn

--- src/app/(app)/index.jsx
+++ src/app/(app)/index.jsx
@@ -2,21 +2,24 @@ import { View, Text, Pressable } from 'react-native';
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useSession } from '../../context/AuthContext';
+import SubmitButton from '../../components/buttons/SubmitButton';
 
-const index = () => {
+const Index = () => {
   const { signOut, session } = useSession();
-  const { user } = JSON.parse(session);
+  // const { user } = JSON.parse(session);
 
-  console.log('session app', session);
   return (
     <SafeAreaView>
+      <Text> User Data: </Text>
       <Text>{session}</Text>
 
-      <Pressable onPress={() => signOut()}>
-        <Text>hola</Text>
-      </Pressable>
+      <SubmitButton
+        title="Sign Out"
+        onPress={() => signOut()}
+        customStyle={{ backgroundColor: 'red' }}
+      />
     </SafeAreaView>
   );
 };
 
-export default index;
+export default Index;
